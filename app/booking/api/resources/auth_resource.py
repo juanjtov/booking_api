7215223372a418ea0be5e359425990aaad31196a
Julian Santos <ingjuliansantos@gmail.com>
2020-09-12 18:55:03 -0500

    Actualizada la arquitectura, se incluyeron archivos no agregados previamente

---
+++ app/booking/api/resources/auth_resource.py
@@ -0,0 +1,42 @@
+import json
+import datetime
+from flask import jsonify, request
+from flask_restful import Resource
+from flask_jwt_extended import create_access_token, jwt_required
+from app.booking.models import User, UserLogin
+from app.db import set_user, get_user_for_login, get_user
+
+class SignupApi(Resource):
+    def post(self):
+        post_data = request.get_json()
+        data_dict = json.loads(json.dumps(post_data))
+
+        user = User(**data_dict)
+        user.hash_password()
+        
+        print('\nUser: ', user.name)
+        
+        e = set_user(user)
+
+        print('Error', e)
+
+        if e == 1062:
+            return {'error':'email already exits'}, 200
+        else:
+            return {'email': user.email, 'msg': 'was created correctly'}, 200
+
+class LoginApi(Resource):
+    def post(self):
+        body = request.get_json()
+        user_dict  = get_user(body.get('email'))[0]
+        
+        user = User(**user_dict)
+        authorized = user.check_password(body.get('password'))
+        if not authorized:
+            return {'error': 'Email or password invalid'}, 401
+
+        expires = datetime.timedelta(days=7)
+        access_token = create_access_token(identity=str(user.id), expires_delta=expires)
+
+        return {'token': access_token}, 200
+    
--- app/db.py
+++ app/db.py
@@ -44,7 +44,7 @@ def set_user(user):
                 "{user.last_name}",\
                 "{user.email}",\
                 "{user.password}",\
-                {user.phone_number},\
+                "{user.phone_number}",\
                 "{user.address}",\
                 "{user.profile_image_url}",\
                 {user.city_id},\
@@ -135,4 +135,50 @@ def get_user_for_login(email):
         connection.close()
         return  result,e
 
+def update_user(user):
+    """
+    param: User object
+    returns the MySQL error handle by the try-except senteces
+    """
+
     
+    connection = pymysql.connect(host=host,
+                             user=db_user,
+                             password=password,
+                             db=db,
+                             charset=charset,
+                             cursorclass=pymysql.cursors.DictCursor)
+    try:
+        with connection.cursor() as cursor:
+            e = 'none'
+
+            print(user.email)
+            print(user.name)
+            print(user.phone_number)
+            # Create a new record
+            sql = f"""UPDATE `users` SET \
+                                        `name` = "{user.name}", \
+                                        `last_name` = "{user.last_name}", \
+                                        `email` = "{user.email}", \
+                                        `phone_number` = "{user.phone_number}", \
+                                        `address` = "{user.address}", \
+                                        `profile_image_url` = "{user.profile_image_url}", \
+                                        `city_id` = {user.city_id}, \
+                                        `account_type_id` = {user.account_type_id}, \
+                                        `lat_location` = {user.lat_location}, \
+                                        `long_location` = {user.long_location}, \
+                                        `active` = {user.active} WHERE users.email = '{user.email}'"""
+            
+            cursor.execute(sql)
+
+        # connection is not autocommit by default. So you must commit to save
+        # your changes.
+        connection.commit()
+
+    except Exception as ex:        
+        print(ex.args[0])
+        print(ex.args[1])
+        e = ex.args[0]
+    finally:
+        connection.close()
+        return e   
