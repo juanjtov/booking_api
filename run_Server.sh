ef1d0802302f8a05ec98571afce728759f781931
Juan Tovar <jujotomo@gmail.com>
2020-09-22 18:36:30 -0700

    Bucket funcionando para instancia gcloud

--- run_Server.sh
+++ run_Server.sh
@@ -6,6 +6,6 @@ source venv/bin/activate
 export FLASK_APP="main.py"
 export FLASK_ENV="development"
 export APP_SETTINGS_MODULE="config.default"
-export CLOUD_STORAGE_BUCKET=api-images-hotels
+export CLOUD_STORAGE_BUCKET=apirest-booking-buck
 
 flask run
